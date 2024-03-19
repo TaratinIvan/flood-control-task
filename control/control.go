2c98a77bd57004965093547ce11f69ecf5188a05
itara <itaratin@mail.ru>
2024-03-20 00:44:59 +0300

    update

---
+++ control/control.go
@@ -0,0 +1,64 @@
+package control
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+	"sync"
+	"time"
+)
+
+type redisFloodControl struct {
+	redisClient *redis.Client
+	maxRequests int
+	windowSize  time.Duration
+	mutex       sync.Mutex
+}
+
+func NewFloodControl(redisClient *redis.Client, maxRequests int, windowSize time.Duration) main.FloodControl {
+	return &redisFloodControl{
+		redisClient: redisClient,
+		maxRequests: maxRequests,
+		windowSize:  windowSize,
+	}
+}
+
+func (f *redisFloodControl) Check(ctx context.Context, userID int64) (bool, error) {
+	f.mutex.Lock()
+	defer f.mutex.Unlock()
+
+	key := fmt.Sprintf("flood_control:%d", userID)
+
+	// Получение списка timestamp-ов
+	timestamps, err := f.redisClient.LRange(ctx, key, 0, -1).Result()
+	if err != nil {
+		return false, err
+	}
+
+	// Если за последние N секунд было совершено больше K вызовов
+	if len(timestamps) >= f.maxRequests {
+		// Вычисление разницы между текущим временем и timestamp-ом в начале списка
+		oldestTimestamp, err := time.Parse(time.RFC3339, timestamps[0])
+		if err != nil {
+			return false, err
+		}
+
+		difference := time.Since(oldestTimestamp)
+
+		// Если разница меньше N, то это означает, что лимит запросов превышен
+		if difference < f.windowSize {
+			return false, nil
+		}
+	}
+
+	// Добавление timestamp-а в список
+	err = f.redisClient.LPush(ctx, key, time.Now().Format(time.RFC3339)).Err()
+	if err != nil {
+		return false, err
+	}
+
+	// Ограничение длины списка
+	f.redisClient.LTrim(ctx, key, 0, f.maxRequests-1)
+
+	return true, nil
+}
--- main.go
+++ main.go
@@ -2,14 +2,44 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"time"
+
+	"github.com/TaratinIvan/flood-control-task/control"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/rs/xid"
 )
 
 func main() {
+	// Подключение к Redis
+	redisClient := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
 
+	// Создание экземпляра FloodControl
+	floodControl := control.NewFloodControl(redisClient, 10, 5)
+
+	// Контекст
+	ctx := context.Background()
+
+	// Имитация вызовов Check
+	for i := 0; i < 20; i++ {
+		userID := xid.New().String()
+
+		allowed, err := floodControl.Check(ctx, userID)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Printf("UserID: %s, Allowed: %v\n", userID, allowed)
+
+		time.Sleep(time.Second)
+	}
 }
 
-// FloodControl интерфейс, который нужно реализовать.
-// Рекомендуем создать директорию-пакет, в которой будет находиться реализация.
+// Интерфейс FloodControl
 type FloodControl interface {
 	// Check возвращает false если достигнут лимит максимально разрешенного
 	// кол-ва запросов согласно заданным правилам флуд контроля.
